424abea564fb68fc9c2b8ce5edd0a1cebd32ebbc
smiadv2022 <msorokina@spacemedia.com.ua>
2023-05-20 13:44:29 +0300

    profiledate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import user from '../data/user.json';
+import { Profile } from './profile/Profile';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,13 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
